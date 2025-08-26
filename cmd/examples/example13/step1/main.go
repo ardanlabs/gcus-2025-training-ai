d9b61f36a91e6197ab3a9a60c7eaeafd1b478a3b
Florin Pățan <florinpatan@gmail.com>
2025-08-26 15:46:24 -0400

    Add keyframe

--- cmd/examples/example13/step1/main.go
+++ cmd/examples/example13/step1/main.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/ardanlabs/ai-training/foundation/client"
+	"github.com/ardanlabs/ai-training/foundation/vector"
 )
 
 type frame struct {
@@ -140,4 +141,194 @@ func getVideoDuration(filePath string) (float64, error) {
 	}
 
 	return duration, nil
+}
+
+// -------------------------------------------------------------------------
+
+func extractKeyFramesFromVideo(source string) error {
+	fmt.Printf("Extracting Video %s keyframes...\n", source)
+	defer fmt.Println("\nDONE Extracting Video keyframes")
+
+	ffmpegCommand := fmt.Sprintf("ffmpeg -skip_frame nokey -i %s -frame_pts true -fps_mode vfr -loglevel error zarf/samples/videos/frames/%%05d.jpg", source)
+
+	out, err := exec.Command("/bin/sh", "-c", ffmpegCommand).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error while running ffmpeg: %w: %s", err, string(out))
+	}
+
+	return nil
+}
+
+// -------------------------------------------------------------------------
+
+const extractFrameInfoPrompt = `
+		Provide a detailed description of this image in 300 words or less.
+		Also, classify this image as: "source code", "diagram", "terminal", or "other" depending on the content it features the most.
+		If icons are present in the middle of the image and blocking the main content, classify them as "icon".
+		
+		Output the text in a valid JSON format MATCHING this format:
+		{
+			"text": "<image description>",
+			"classification": "<image classification>"
+		}
+
+		Encode any special characters in the JSON output.
+		
+		Make sure that there's no extra whitespace or formatting, or markdown surrounding the json output.
+		MAKE SURE THAT THE JSON IS VALID.
+		DO NOT INCLUDE ANYTHING ELSE BUT THE JSON DOCUMENT IN THE RESPONSE.
+`
+
+func processChunk(ctx context.Context, llmChat *client.LLM, llmEmbed *client.LLM, sourceDir string, sourceFileName string, totalFramesTime float64, duration float64) error {
+	fullPath := filepath.Join(sourceDir, "chunks", sourceFileName)
+
+	fmt.Printf("\nRemoving the frames from the previous chunk: %s\n", filepath.Join(sourceDir, "frames"))
+
+	previousFrames, err := fs.Glob(os.DirFS(sourceDir), "frames/*")
+	if err != nil {
+		return fmt.Errorf("glob: %w", err)
+	}
+
+	for _, previousFrame := range previousFrames {
+		if err := os.Remove(filepath.Join(sourceDir, previousFrame)); err != nil {
+			return fmt.Errorf("remove previous frame: %w", err)
+		}
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Printf("\nProcessing video chunk: %s\n", fullPath)
+
+	if err := extractKeyFramesFromVideo(fullPath); err != nil {
+		return fmt.Errorf("process video: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Printf("\nProcessing images in directory: %s\n", sourceDir)
+
+	files, err := getFilesFromDirectory(filepath.Join(sourceDir, "frames"))
+	if err != nil {
+		return fmt.Errorf("get files from directory: %w", err)
+	}
+
+	frames := make([]frame, 0, len(files))
+
+	for _, fileName := range files {
+		f := frame{
+			fileName:  fileName,
+			startTime: totalFramesTime,
+			duration:  duration,
+		}
+
+		fmt.Printf("\nProcessing image: %s\n", fileName)
+
+		// -------------------------------------------------------------------------
+
+		image, mimeType, err := readImage(fileName)
+		if err != nil {
+			return fmt.Errorf("read image: %w", err)
+		}
+
+		f.mimeType = mimeType
+		f.image = make([]byte, len(image))
+		copy(f.image, image)
+
+		// -------------------------------------------------------------------------
+
+		fmt.Println("\nGenerating embeddings for the image description:")
+
+		embedding, err := llmEmbed.EmbedWithImage(ctx, "", image, mimeType)
+		if err != nil {
+			return fmt.Errorf("llm.EmbedText: %w", err)
+		}
+
+		fmt.Printf("%v...%v\n", embedding[0:3], embedding[len(embedding)-3:])
+
+		f.embedding = make([]float64, dimensions)
+		copy(f.embedding, embedding)
+
+		// -------------------------------------------------------------------------
+
+		frames = append(frames, f)
+	}
+
+	// -------------------------------------------------------------------------
+
+	var uniqueFrames []frame
+	for idx, f := range frames {
+		if idx == 0 {
+			uniqueFrames = append(uniqueFrames, f)
+			continue
+		}
+
+		var isDuplicate bool
+		for _, previousFrame := range uniqueFrames {
+			fmt.Printf("Checking image similarity between: %s - %s\n", previousFrame.fileName, f.fileName)
+			similarity := vector.CosineSimilarity(previousFrame.embedding, f.embedding)
+			fmt.Printf("  - Image similarity: %.3f\n", similarity)
+
+			if similarity > similarityThreshold {
+				isDuplicate = true
+				fmt.Println("  - Image is similar to previous image")
+				break
+			}
+		}
+
+		if !isDuplicate {
+			uniqueFrames = append(uniqueFrames, f)
+		}
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Printf("\nUnique Frames: %d\n", len(uniqueFrames))
+	for _, f := range uniqueFrames {
+		fmt.Printf("  - FileName: %s - [%.4f, %.4f]\n", f.fileName, f.startTime, f.duration)
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Println("\nExtracting frame descriptions:")
+
+	for i, f := range uniqueFrames {
+		fmt.Printf("Extracting description for image: %s\n", f.fileName)
+
+		description, err := llmChat.ChatCompletions(ctx, extractFrameInfoPrompt, client.WithImage(f.mimeType, f.image))
+		if err != nil {
+			return fmt.Errorf("chat completions: %w", err)
+		}
+
+		description = strings.Trim(description, "`")
+		description = strings.TrimPrefix(description, "json")
+
+		fmt.Printf("LLM RESPONSE: %s\n", description)
+
+		var descr struct {
+			Text           string `json:"text"`
+			Classification string `json:"classification"`
+		}
+		if err := json.Unmarshal([]byte(description), &descr); err != nil {
+			return fmt.Errorf("unmarshal: %w", err)
+		}
+
+		if descr.Classification == "icon" {
+			fmt.Println("  - Icon classification detected, skipping...")
+			continue
+		}
+
+		uniqueFrames[i].description = descr.Text
+		uniqueFrames[i].classification = descr.Classification
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Println("\nUnique Frames:")
+
+	for _, f := range uniqueFrames {
+		fmt.Printf("\t- FileName: %s - [%.4f, %.4f, %s]\n", f.fileName, f.startTime, f.startTime, f.classification)
+	}
+
+	fmt.Println("\nDONE")
+	return nil
 }
